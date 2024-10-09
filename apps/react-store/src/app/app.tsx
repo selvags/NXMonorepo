078a9ffbacf191deed33cc97e52d7ded415218e8
Subramanian <selvags199@gmail.com>
2024-10-09 18:21:11 +0530

    updated home component

--- apps/react-store/src/app/app.tsx
+++ apps/react-store/src/app/app.tsx
@@ -5,11 +5,19 @@ import NxWelcome from './nx-welcome';
 import { Products } from '@nx-mono-repo/products';
 import { Orders } from '@nx-mono-repo/orders';
 
+const HomeComponent = () => (
+  <>
+    <NxWelcome title='react-store'/>
+    <Products />
+    <Orders />
+  </>
+)
+
 export function App() {
   return (
     <>
       <Routes>
-        <Route path="/NXMonorepo" element={(<NxWelcome title='react-store'/>)}/>
+        <Route path="/NXMonorepo" element={(<HomeComponent />)}/>
         <Route path="/NXMonorepo/products" element={(<Products />)}/>
         <Route path='/NXMonorepo/orders' element={<Orders />} />
       </Routes>
